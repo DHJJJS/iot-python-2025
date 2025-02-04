c453e8f1e4472d5f715623a506dfbb01301da0da
DHJJJS <lowknjua@gmail.com>
2025-02-04 09:37:54 +0900

    First Commit

---
+++ day01/py01_first.py
@@ -0,0 +1,10 @@
+# 주석입니다. 주석은 프로그램 실행에 아무런 영향을 미치지 않습니다.
+# 맘껏 적으세요.
+
+print('파이썬 시작!!!') # 무조건 Ctrl + S로 저장을 습관화!
+print('Hello Python!')
+
+# Hello world!가 사용된 건 1978년에 출판된 "The C Programming Language"에서 비롯됨.
+
+# 실행은 Ctrl + F5(디버깅 없이 실행)
+# 터미널의 PS는 PowerShell이다. 터미널의 기본 콘솔 자체가 PS이기 때문에.
